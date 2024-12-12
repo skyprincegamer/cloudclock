f7a5c2661ef8846a85cf8ebd632ad32bf27405f4
skyprincegamer <akgamer3108@gmail.com>
2024-12-12 10:34:14 +0530

    initial commit

---
+++ frontend/src/main.js
@@ -0,0 +1,31 @@
+const { invoke } = window.__TAURI__.core;
+
+
+function setTimer(){
+var timeMsgBox= document.querySelector("#time-msg");
+
+timeMsgBox.innerHTML = document.querySelector("#targetDate").value.toString();
+}
+ function updateClock ( clock ) {
+    clock.innerHTML = new Date().toLocaleTimeString();
+  }
+
+function showTime() {
+   setInterval(function () {
+      updateClock( timeMsgBox);
+  }, 1000);
+
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+ var timeMsgBox= document.querySelector("#time-msg");
+  document.querySelector("#time-button").addEventListener("click", (e) => {
+    showTime();
+    document.querySelector("#time-button").removeEventListener("show-time");
+  });
+});
+document.querySelector("#set-timer").addEventListener("click" , (e) => 
+{
+  setTimer();
+
+  });
