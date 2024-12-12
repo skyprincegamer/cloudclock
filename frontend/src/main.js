79709b5b0f9fe02b3eea631741bc8b61d3936797
skyprincegamer <akgamer3108@gmail.com>
2024-12-12 15:47:32 +0530

    clean js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,31 +1,29 @@
 const { invoke } = window.__TAURI__.core;
 
 
-function setTimer(){
-var timeMsgBox= document.querySelector("#time-msg");
+function setTimer() {
+   var timeMsgBox = document.querySelector("#target-date");
 
-timeMsgBox.innerHTML = document.querySelector("#targetDate").value.toString();
+   timeMsgBox.innerHTML = document.querySelector("#targetDate").value.toString();
 }
- function updateClock ( clock ) {
-    clock.innerHTML = new Date().toLocaleTimeString();
-  }
+function updateClock(clock) {
+   clock.innerHTML = new Date().toLocaleTimeString();
+}
 
 function showTime() {
-   setInterval(function () {
-      updateClock( timeMsgBox);
-  }, 1000);
+   setInterval(function() {
+      updateClock(timeMsgBox);
+   }, 1000);
 
 }
 
 window.addEventListener("DOMContentLoaded", () => {
- var timeMsgBox= document.querySelector("#time-msg");
-  document.querySelector("#time-button").addEventListener("click", (e) => {
-    showTime();
-    document.querySelector("#time-button").removeEventListener("show-time");
-  });
+   document.querySelector("#time-button").addEventListener("click", () => {
+      showTime();
+      document.querySelector("#time-button").removeEventListener("show-time");
+   });
 });
-document.querySelector("#set-timer").addEventListener("click" , (e) =>
-{
-  setTimer();
+document.querySelector("#set-timer").addEventListener("click", () => {
+   setTimer();
 
-  });
+});
