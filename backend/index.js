4d94395fced09269165baa7ac0d0b128062343a6
skyprincegamer <akgamer3108@gmail.com>
2024-12-20 12:28:57 +0530

    user push works

--- backend/index.js
+++ backend/index.js
@@ -7,13 +7,22 @@ let users;
     users = JSON.parse(await fs.readFile('users.json', { encoding: 'utf8' }));
     console.log(users)
   } catch (err) {
-    console.log(err);
+    users=[];
+    await fs.writeFile('users.json' , "[]" )
   }
 }
 fetchUsers();
+function findUser(id){
+  for(const element of users){
+    if (element.id === id){
+      return element
+    }
+  }
+  return undefined;
+}
 async function addUser(user) {
   users.push(user);
-  await fs.writeFile('users.json' , JSON.stringify(users)) ;
+  await fs.writeFile('users.json' , JSON.stringify(users));
 }
 // respond with "hello world" when a GET request is made to the homepage
 app.use((req, res, next) => {
@@ -26,21 +35,36 @@ app.use(express.json())
 app.get('/users/:id', (req, res) => {
   res.send(users[req.params.id - 1 ].name);
 })
+app.get('/alarms/:user/:alarm' , (req , res ) => {
+if(findUser(req.params.user) !== undefined){
+  if(findAlarm(req.params.alarm , req.params.user) !== undefined){
+    return res.send('Alarm is set');
+  }
+}
 
+});
+app.post('/alarms/:user/' , (req, res) => {
+let user = findUser(req.params.user);
+let id ;
+if(user !== undefined){
+  if(user.alarms === undefined){
+    user.alarms =[];
+  }
+  id = users.alarms.length
+  user.alarms.push({ "id" : id, "date" : req.body.date });
+  console.log(user.alarms);
+  return res.send(JSON.stringify(user.alarms[user.alarms.length-1]));
+}
+}
+);
 app.post('/users' ,(req , res) => {
-  console.log(req.body);
   let newUser = req.body;
-  if(users === undefined){
-    users = [];
-    newUser.id = 0;
-  }
-  else{
-    newUser.id = users.splice(-1)[0].id +1;
-  }
+  newUser.id = users.length;
   addUser(newUser);
+
   
   
-  return res.send('New user id is ' + newUser.id);
+  return res.send(newUser.id.toString());
   
 })
 app.listen(6969, () => {
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,7 @@ const { invoke } = window.__TAURI__.core;
 //    document.getElementById("server-data").innerHTML = await response.text();
 
 // }
+let userID;
 async function sendData() {
 
    const response = await fetch('http://localhost:6969/' , {
@@ -53,6 +54,23 @@ function showTime() {
    }, 1000);
 
 }
+async function addAlarm() {
+   const alarm = {
+      date : document.querySelector("#targetDate").value
+   }
+   const response = await fetch("http://localhost:6969/alarms/" + userID +'/' , 
+      {
+         method: "POST",
+         headers : {
+            "Content-Type" : "application/json"
+         },
+         body : JSON.stringify(alarm)
+      }
+      
+    );
+    console.log(await response.text());
+}
+
 async function addUser() {
    const data = {
       name :"tauri",
@@ -65,10 +83,12 @@ async function addUser() {
       },
       body : JSON.stringify(data)
    });
-   console.log(await response.text());
+   userID = await response.text();
+   console.log(userID);
 }
 
 
 document.querySelector("#time-button").addEventListener("click",  event => {event.preventDefault();showTime();});
 document.querySelector("#set-timer").addEventListener("click",  event => {event.preventDefault();setTimer();});
 document.getElementById("send-data").addEventListener("click" , event => {event.preventDefault();addUser();});
+document.getElementById("send-alarm").addEventListener("click" , event => {event.preventDefault();addAlarm();});
