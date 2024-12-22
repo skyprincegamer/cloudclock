0ebbeee9cb007226f90dfea9c99aa220a19b74f4
skyprincegamer <akgamer3108@gmail.com>
2024-12-22 21:01:33 +0530

    remove bs

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,62 +1,10 @@
 const { invoke } = window.__TAURI__.core;
 
-
-// async function fetchData() {
-//    const response = await fetch("http://localhost:6969/");
-//    document.getElementById("server-data").innerHTML = await response.text();
-
-// }
 let userID;
-async function sendData() {
 
-   const response = await fetch('http://localhost:6969/' , {
-      method : 'POST',
-      headers : {
-         "Content-Type" : "text/plain"
-      },
-      body : timeBox.value.toString()
-   });
-   console.log(response);
-
-}
-
-function timeUp() {
-   document.getElementById("time-up").innerHTML = "TIME UP!!!";
-}
-function setTimer() {
-   
-   var timeBox = document.querySelector("#targetDate");
-   
-   if (timeBox.value.toString() === "")
-      return;
-   
-   const timerDate = new Date(timeBox.value.toString());
-   var mili = timerDate.getTime() - (new Date()).getTime() ;
-   
-   if (mili < 0)
-      return;
-   
-   setTimeout(timeUp, mili);
-}
-
-
-function updateClock(clock) {
-
-   clock.innerHTML = new Date().toLocaleTimeString();
-
-}
-
-function showTime() {
-
-   var timeMsgBox = document.querySelector("#time-msg");
-   setInterval(function() {
-      updateClock(timeMsgBox);
-   }, 1000);
-
-}
-async function addAlarm() {
+async function addAlarm(targetDate) {
    const alarm = {
-      date : document.querySelector("#targetDate").value
+      date : targetDate
    }
    const response = await fetch("http://localhost:6969/alarms/" + userID +'/' , 
       {
@@ -73,8 +21,9 @@ async function addAlarm() {
 
 async function addUser() {
    const data = {
-      name :"tauri",
-      pass: 'node'
+      name :document.querySelector('[name="username"]'),
+      pass: document.querySelector('[name="password"]'),
+      serverpass : document.querySelector('[name="server-password"]')
    }
    const response = await fetch('http://localhost:6969/users' , {
       method : 'POST',
@@ -87,8 +36,4 @@ async function addUser() {
    console.log(userID);
 }
 
-
-document.querySelector("#time-button").addEventListener("click",  event => {event.preventDefault();showTime();});
-document.querySelector("#set-timer").addEventListener("click",  event => {event.preventDefault();setTimer();});
-document.getElementById("send-data").addEventListener("click" , event => {event.preventDefault();addUser();});
-document.getElementById("send-alarm").addEventListener("click" , event => {event.preventDefault();addAlarm();});
+document.getElementById("login").addEventListener("click" , event => {event.preventDefault();addUser();});
