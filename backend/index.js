a5af222d78eca05a9babcf71fa57998501dd9def
skyprincegamer <akgamer3108@gmail.com>
2024-12-22 19:37:34 +0530

    feat : alarm delete

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const fs = require('node:fs/promises')
 const app = express()
 let users;
+
+
+
  async function fetchUsers() {
   try {
     users = JSON.parse(await fs.readFile('users.json', { encoding: 'utf8' }));
@@ -11,7 +14,10 @@ let users;
     await fs.writeFile('users.json' , "[]" )
   }
 }
+
 fetchUsers();
+
+
 function findUser(id){
   id = Number(id);
   for(const element of users){
@@ -21,51 +27,98 @@ function findUser(id){
   }
   return undefined;
 }
+function findAlarm(alarmID , userID){
+  user = findUser(userID);
+  if(user!== undefined){
+    for(const alarm of user.alarms)
+    {
+      if(alarm.id == alarmID)
+        return alarm;
+    
+    }
+    return undefined;
+}
+  else 
+    return undefined;
+}
+
+
 async function addUser(user) {
   users.push(user);
   await fs.writeFile('users.json' , JSON.stringify(users));
 }
-// respond with "hello world" when a GET request is made to the homepage
+
+
+
+// headers
 app.use((req, res, next) => {
   res.append('Access-Control-Allow-Origin', ['*']);
   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.append('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
+
 app.use(express.json())
+
 app.get('/users/:id', (req, res) => {
   res.send(users[req.params.id - 1 ].name);
 })
+
 app.get('/alarms/:user/:alarm' , (req , res ) => {
-if(findUser(req.params.user) !== undefined){
+
   if(findAlarm(req.params.alarm , req.params.user) !== undefined){
     return res.send('Alarm is set');
   }
-}
 
 });
+
+
+
+
 app.post('/alarms/:user/' , (req, res) => {
-let user = findUser(req.params.user);
-console.log(users + ', params = ' + req.params.user )
-console.log(user)
-if(user !== undefined){
-  if(user.alarms === undefined){
-    user.alarms =[];
+
+  let user = findUser(req.params.user);
+  console.log(users + ', params = ' + req.params.user )
+  console.log(user)
+  if(user !== undefined){
+
+    if(user.alarms === undefined){
+      user.alarms =[];
+    }
+
+    const id = user.alarms.length
+    user.alarms.push({ "id" : id, "date" : req.body.date });
+    console.log(user.alarms);
+    return res.send(JSON.stringify(user.alarms[user.alarms.length-1]));
   }
-  const id = user.alarms.length
-  user.alarms.push({ "id" : id, "date" : req.body.date });
-  console.log(user.alarms);
-  return res.send(JSON.stringify(user.alarms[user.alarms.length-1]));
-}
-return res.send("ERROR")
-}
-);
+
+return res.send("ERROR")  
+});
+
+
+app.delete("/alarms/:user/:alarm" , (req, res) =>{
+  
+    if(findAlarm(req.params.alarm , req.params.user) !== undefined){
+      alarm = findAlarm(req.params.alarm , req.params.user);
+      user.alarms = user.alarms.filter(function (a) {
+        return a != alarm;
+      });
+      return res.send("Successfully deleted alarm" + JSON.stringify(alarm));
+    }
+  
+});
+
+
+
+
 app.post('/users' ,(req , res) => {
   let newUser = req.body;
   newUser.id = users.length;
   addUser(newUser);
   return res.send(newUser.id.toString());
   
+
+
 })
 app.listen(6969, () => {
   console.log(`Example app listening on port ${6969}`)
